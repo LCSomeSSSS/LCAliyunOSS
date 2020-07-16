e1ecbb4fb44eb1460d4b71b336e3e9cc19c5469e
Edison <1733545107@qq.com>
2020-07-16 22:40:41 +0800

    add files

---
+++ OSS上传/OSS上传/test.m
@@ -0,0 +1,20 @@
+//
+//  test.m
+//  OSS上传
+//
+//  Created by Edison on 2020/7/16.
+//  Copyright © 2020 edison. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+-(instancetype)init{
+    if (self = [super init]) {
+        NSLog(@"hello world");
+    }
+    return self;
+}
+
+@end
